8a64f2ab374e6767248e79ea1053cc9502e8bc3d
Hengjun Zhang <azhang364@gmail.com>
2026-05-11 13:18:52 -0400

    Initial Commit

---
+++ VTM/VTMApp.swift
@@ -0,0 +1,17 @@
+//
+//  VTMApp.swift
+//  VTM
+//
+//  Created by 张恒郡 on 2026-05-11.
+//
+
+import SwiftUI
+
+@main
+struct VTMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
